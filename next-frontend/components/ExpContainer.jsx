ab6335130af0040e685a3d41ccedae190170d2d2
Michael Johnson <hack.reactor.mj0730@gmail.com>
2020-11-14 23:05:47 -0600

    add custom _document to handle ssr for mui styling; add some styling to expContainer; added disable/enable to buttons

--- next-frontend/components/ExpContainer.jsx
+++ next-frontend/components/ExpContainer.jsx
@@ -1,39 +1,56 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ExpLineItem from './ExpLineItem';
+
+//Components
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import DeleteIcon from '@material-ui/icons/Delete';
+
+//Icons
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+
+//Style
 import styles from '../styles/ExpInput.module.css';
 
 const ExpContainer = () => {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(2);
   const [inputList, setInputList] = useState([<ExpLineItem key='1' />]);
+  const [disableDelete, setDisableDelete] = useState();
+  const [disableAdd, setDisableAdd] = useState();
 
-  const addInput = () => {
-    if (inputList.length == 5) {
-      alert('Please add only the five most recent facilities.');
-      return;
+  useEffect(() => {
+    inputList.length == 1 ? setDisableDelete(true) : setDisableDelete(false);
+    inputList.length == 5 ? setDisableAdd(true) : setDisableAdd(false);
+  }, [inputList]);
+
+  function addInput() {
+    if (inputList.length < 5) {
+      setCount(count + 1);
+      setInputList(inputList.concat(<ExpLineItem key={count} />));
     }
-    setCount(count + 1);
-    setInputList(inputList.concat(<ExpLineItem key={count + 1} />));
-  };
+  }
 
-  const deleteInput = () => {
-    if (inputList.length == 1) return;
-    const temp = inputList.slice('');
-    temp.pop();
-    setInputList(temp);
-  };
+  function deleteInput() {
+    if (inputList.length > 1) {
+      const temp = inputList.slice('');
+      temp.pop();
+      setCount(count - 1);
+      setInputList(temp);
+    }
+  }
 
   return (
-    <div>
+    <Box>
       {inputList}
-      <div>
-        <Button onClick={addInput}>add more experience</Button>
-        <Button startIcon={<DeleteIcon />} onClick={() => deleteInput()}>
-          delete
+      <Box display='flex' justifyContent='space-between'>
+        <Button startIcon={<AddCircleOutlineIcon />} color='default' onClick={addInput} disabled={disableAdd}>
+          Add Experience
         </Button>
-      </div>
-    </div>
+        <Button startIcon={<HighlightOffIcon />} color='default' onClick={() => deleteInput()} disabled={disableDelete}>
+          Delete
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
---
+++ next-frontend/components/OfferContainer.jsx
@@ -0,0 +1,48 @@
+import { Button, Container } from '@material-ui/core';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import OfferLineItem from './OfferLineItem';
+
+function OfferContainer() {
+  const [count, setCount] = useState(6);
+  const [inputList, setInputList] = useState([
+    <OfferLineItem key='1' />,
+    <OfferLineItem key='2' />,
+    <OfferLineItem key='3' />,
+    <OfferLineItem key='4' />,
+    <OfferLineItem key='5' />,
+  ]);
+
+  useEffect(() => {
+    inputList.length == 1 ? setDisableDelete(true) : setDisableDelete(false);
+    inputList.length == 5 ? setDisableAdd(true) : setDisableAdd(false);
+  }, [inputList]);
+
+  function addInput() {
+    if (inputList.length < 5) {
+      setCount(count + 1);
+      setInputList(inputList.concat(<ExpLineItem key={count} />));
+    }
+  }
+
+  function deleteInput() {
+    if (inputList.length > 1) {
+      const temp = inputList.slice('');
+      temp.pop();
+      setCount(count - 1);
+      setInputList(temp);
+    }
+  }
+
+  return (
+    <Container>
+      {inputList}
+      <Button startIcon={<AddCircleOutlineIcon />} onClick={addInput}>
+        Add More
+      </Button>
+      <Button startIcon={<HighlightOffIcon />} onClick={() => deleteInput()}>
+        Delete
+      </Button>
+    </Container>
+  );
+}
