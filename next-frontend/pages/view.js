3c2b64bd6a2daed1ca30dd9ad8f603a5591560c5
Michael Johnson <mrj0730@icloud.com>
2020-12-15 01:27:47 -0600

    add view page; eslint config for next10

---
+++ next-frontend/pages/view.js
@@ -0,0 +1,15 @@
+function View() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ipsum totam fuga accusamus doloremque, sequi
+      necessitatibus consequuntur libero vel impedit minus amet in voluptatibus id consequatur, explicabo eum soluta
+      cumque!
+    </p>
+  );
+}
+
+export async function getStaticProps() {
+  return { props: {} };
+}
+
+export default View;
