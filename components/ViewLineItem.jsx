b847e53610e27b85a99bb45f161f57cfaddf784e
Michael Johnson <mrj0730@icloud.com>
2021-02-23 14:45:52 -0600

    cleanup; reorder prev exp chips so facility id is first

--- components/ViewLineItem.jsx
+++ components/ViewLineItem.jsx
@@ -1,12 +1,33 @@
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import { Box, Card, CardContent, Chip, Container, Divider, Typography } from '@material-ui/core';
-import styles from '../styles/ViewLineItem.module.css';
 import uniqid from 'uniqid';
 
+const useStyles = makeStyles((theme) => ({
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+
+  muiCard: {
+    marginBottom: '1em',
+    backgroundColor: '#f8f8ff',
+  },
+  muiChip: {
+    marginRight: theme.spacing(0.5),
+    minWidth: 73,
+  },
+  muiBox: {
+    marginBottom: theme.spacing(1),
+  },
+}));
+
 function ViewLineItem({ item }) {
+  const classes = useStyles();
+
   return (
     <Container>
-      <Card varient='outlined' className={styles.muiCard}>
+      <Card varient='outlined' className={classes.muiCard}>
         <CardContent>
           <Typography varient='h3' gutterBottom>
             {item.offerYear}
@@ -17,10 +38,28 @@ function ViewLineItem({ item }) {
               Experience
             </Typography>
             {item.inputData.map((data) => (
-              <Box className={styles.muiBox} key={uniqid()}>
-                <Chip variant='outlined' color='primary' size='small' label={data.agency} />
-                <Chip variant='outlined' color='primary' size='small' label={data.facility.toUpperCase()} />
-                <Chip variant='outlined' color='primary' size='small' label={data.type} />
+              <Box className={classes.muiBox} key={uniqid()}>
+                <Chip
+                  className={classes.muiChip}
+                  variant='outlined'
+                  color='primary'
+                  size='small'
+                  label={data.facility.toUpperCase()}
+                />
+                <Chip
+                  className={classes.muiChip}
+                  variant='outlined'
+                  color='primary'
+                  size='small'
+                  label={data.agency.toUpperCase()}
+                />
+                <Chip
+                  className={classes.muiChip}
+                  variant='outlined'
+                  color='primary'
+                  size='small'
+                  label={data.type.toUpperCase()}
+                />
               </Box>
             ))}
           </Box>
@@ -32,7 +71,7 @@ function ViewLineItem({ item }) {
               Offers
             </Typography>
             {item.offerData.map((data) => (
-              <Chip key={uniqid()} color='primary' size='small' label={data} />
+              <Chip key={uniqid()} className={classes.muiChip} color='primary' size='small' label={data} />
             ))}
           </Box>
         </CardContent>
