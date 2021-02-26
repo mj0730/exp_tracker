1c6d1f629f95a34cab9075f9d7d1b2b91be7029b
Michael Johnson <mrj0730@icloud.com>
2021-02-26 17:57:53 -0600

    add home page with navigation

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,49 @@
+import Link from 'next/link';
+import { Card, CardContent, Grid, Typography } from '@material-ui/core';
 import NavDrawer from '../components/NavDrawer';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import PageviewIcon from '@material-ui/icons/Pageview';
+import styles from '../styles/Home.module.css';
 
 function Home() {
   const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
 
   return (
-    <main>
+    <div>
       <NavDrawer disableBackdropTransition={!iOS} disableDiscovery={iOS} />
-    </main>
+      <h1 className={styles.title}>Previous Experience Tracker</h1>
+      <Grid container direction='row' justify='center' alignItems='center' spacing={3}>
+        <Grid item>
+          <Link href='/add'>
+            <Card className={styles.homeCard}>
+              <CardContent>
+                <Typography className={styles.cardTitle} gutterBottom>
+                  <AddBoxIcon style={{ fontSize: '5rem' }} />
+                  <Typography className={styles.cardText} varient='caption'>
+                    Add Your Data
+                  </Typography>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Link>
+        </Grid>
+
+        <Grid item>
+          <Link href='/view'>
+            <Card className={styles.homeCard}>
+              <CardContent>
+                <Typography className={styles.cardTitle}>
+                  <PageviewIcon style={{ fontSize: '5rem' }} />
+                  <Typography className={styles.cardText} varient='caption'>
+                    View Database
+                  </Typography>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Link>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
