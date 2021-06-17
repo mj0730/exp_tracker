ecc7235b9485a97ba2f3c4032a8844a2c955a62e
Michael Johnson <mrj0730@icloud.com>
2021-06-17 15:29:36 -0500

    add next config to disable eslint at build time

---
+++ next.config.js
@@ -0,0 +1,7 @@
+export default {
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
